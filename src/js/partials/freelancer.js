18a5dcca7caf1dbccbb0a029fdb3b5a15fc531e0
mp <mp@jubbr.com>
2016-04-03 16:07:09 +0300

    Hide function for btn
    Hide function for btn

--- src/js/partials/freelancer.js
+++ src/js/partials/freelancer.js
@@ -15,10 +15,21 @@ $(function() {
     });
 });
 
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 0) {
+            $('#up-btn').fadeIn();
+        } else {
+            $('#up-btn').fadeOut();
+        }
+    });
+
+});
+
 // Floating label headings for the contact form
 $(function() {
     $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
+        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
     }).on("focus", ".floating-label-form-group", function() {
         $(this).addClass("floating-label-form-group-with-focus");
     }).on("blur", ".floating-label-form-group", function() {
